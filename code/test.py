ad8b769347e17fa82e876f4ca47ac82239932b85
basgoncalves <basilio.goncalves7@gmail.com>
2025-06-16 10:58:58 +0200

    Add XML templates for calibration and excitation generation; update test script for subject formatting
    - Created a new XML template for calibration setup (`calibration_setup.xml`) with subject and configuration file paths.
    - Added excitation generator XML templates for left (`excitationGenerator_l.xml`), right (`excitationGenerator_r.xml`), and a general version (`excitationGenerator.xml`) with specified input signals and mappings.
    - Updated the test script (`test.py`) to utilize string formatting for subject identifiers in file naming.

--- code/test.py
+++ code/test.py
@@ -1,4 +1,5 @@
-from msk_modelling_python import classes
-import os
+template = 'Results_SO_and_MA_{subject}_StaticOptimization_force.sto'
 
-# select file
+# Using named arguments
+subject_string_format = template.format(subject='003')
+print(f"Using .format() with named argument: {subject_string_format}")
